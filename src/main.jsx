22f6cf12ec6ed3a5d588c37dc6153aaa0b2aab78
ADRIANCHO207 <adriancamargo69@gmail.com>
2025-06-05 07:38:18 -0500

    arreglos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './assets/styles/main.css' // ✅ Importa tu CSS aquí
 
 ReactDOM.createRoot(document.getElementById('root')).render(
